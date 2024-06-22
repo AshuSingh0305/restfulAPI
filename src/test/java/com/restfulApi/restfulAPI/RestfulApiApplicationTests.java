2d8c5dc5de623def33c7f384db2c77b537b5cb81
Ashu <ashu.singh2001.as@gmail.com>
2024-06-22 19:54:24 +0530

    Initial Commit of restful API project

---
+++ src/test/java/com/restfulApi/restfulAPI/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulApi.restfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
